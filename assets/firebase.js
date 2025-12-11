0a35d8de46094e269acb64323ee87e02b530ac38
multazamzakaria-blip <multazamzakaria@gmail.com>
2025-12-11 09:44:32 +0700

    Update firebase.js

--- assets/firebase.js
+++ assets/firebase.js
@@ -5,12 +5,12 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-
 const firebaseConfig = {
   apiKey: "AIzaSyA_kvyDshMXYNqj87bqXZC8tKAi_Kko2Rs",
   authDomain: "rabapp-520b5.firebaseapp.com",
+  databaseURL: "https://rabapp-520b5-default-rtdb.firebaseio.com",
   projectId: "rabapp-520b5",
   storageBucket: "rabapp-520b5.firebasestorage.app",
   messagingSenderId: "1060443577862",
   appId: "1:1060443577862:web:6618775410d09ae16cad87"
 };
 
-// Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
